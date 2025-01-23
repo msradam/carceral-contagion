aa1cdf17726443972879911c1ccaef25d3f22d9e
Adam Munawar Rahman <msrahmanadam@gmail.com>
2025-01-23 16:05:20 -0500

    Update run.py
    Remove hardcoded PORT

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from cc_network.server import server
 import os
 
-server.port = int(os.environ.get("PORT", 5000))
+server.port = int(os.environ.get("PORT"))
 
 if __name__ == '__main__':
     server.launch()
